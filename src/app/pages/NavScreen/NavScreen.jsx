4ec1bad7b294e818b8c1d6994f61e5e008eb0b95
D Evans <z3464512@student.unsw.edu.au>
2017-10-10 19:51:12 +1100

    [NavScreen][Damian/Jae-Hee] Scroll list working with tests

---
+++ src/app/pages/NavScreen/NavScreen.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  string,
+  arrayOf,
+  number,
+  shape,
+} from 'prop-types';
+
+import ScrollList from '../../../framework/components/ScrollList/ScrollList';
+import WithButtonConfigs from '../../../framework/containers/WithButtonConfigs';
+import ButtonAction from '../../../framework/util/ButtonAction';
+
+
+export const NavScreenComponent = ({ locations, selectedIndex }) => {
+  return (
+    <div className='nav-screen'>
+      <h1 className='title'>Navigation!</h1>
+      <ScrollList
+        labels={ locations.map(l => l.name) }
+        selectedIndex={ selectedIndex }
+      />
+    </div>
+  );
+};
+
+const getNextIndex = (indexChange, selectedIndex, locations) => {
+  const newIndex = selectedIndex + indexChange;
+  return Math.abs((newIndex + locations.length) % locations.length);
+};
+
+export const NavScreenButtons = ({ locations, selectedIndex = 0 }) => ({
+  TOP: () => {
+    ButtonAction.goToPage({ state: { selectedIndex: getNextIndex(-1, selectedIndex, locations) } });
+  },
+  BOTTOM: () => {
+    ButtonAction.goToPage({ state: { selectedIndex: getNextIndex(1, selectedIndex, locations) } });
+  },
+  LEFT: () => ButtonAction.goToPage('/'),
+  RIGHT: () => ButtonAction.goToPage('/'),
+  SCREEN: () => ButtonAction.doNothing(),
+});
+
+NavScreenComponent.propTypes = {
+  selectedIndex: number,
+  locations: arrayOf(shape({
+    name: string,
+  })).isRequired,
+};
+
+NavScreenComponent.defaultProps = {
+  selectedIndex: 0,
+};
+
+export default WithButtonConfigs(NavScreenComponent, NavScreenButtons);
